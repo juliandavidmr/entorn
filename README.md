1fe9f0def0e30c0552cd59b29da52b9d1d545b65
Julian David <anlijudavid@hotmail.com>
2016-07-18 15:04:47 -0500

    more functions

--- README.md
+++ README.md
@@ -12,13 +12,16 @@ $ npm install entorn --save
 
 ```javascript
 
-var entorn = require('entorn');
 
 console.log("List all environment variables:" + entorn.list());
 
+console.log("CPUs: ", entorn.cpus); // list of all CPU
+
+console.log("Free memory: ", entorn.free);
+
 console.log("Similary: ", entorn.similary("NODE")); // list of all variables similar environment
 
-console.log("have java installed? " + entorn.hasJavaHome());  // true | false
+console.log("have java installed? " + entorn.hasJavaHome()); // true | false
 
 console.log("where java is installed? " + entorn.getJavaHome()); // string dir folder java
 
@@ -31,4 +34,26 @@ console.log("has axs?", entorn.has("axs")); // true | false
 console.log("or, has SESSION?", entorn.has("SESSION")); // true | false
 
 console.log("get NODE_PATH", entorn.get("NODE_PATH")); // string dir folder
+
+//console.log("Set: ", entorn.set("YOUR_ENV_VAR", "ANY VALUES"));
+
+console.log("CPUs: ", entorn.cpus);
+
+console.log("Free memory: ", entorn.free);
+
+console.log("Network Interfaces: ", entorn.networkInterfaces);
+
+console.log("or Network Interfaces: ", entorn.ni);
+
+console.log("Platform: ", entorn.platform); // 'aix', 'darwin', 'freebsd', 'linux', 'openbsd', 'sunos', 'win32'
+
+console.log("Total memory: ", entorn.totalmem);
+
+console.log("Current folder: ", entorn.current); // Returns the current working directory of the Node.js process.  Similiar to command "pwd" on bash linux
+
+console.log("Version nodejs: ", entorn.version);
+
+console.log("or Version nodejs: ", entorn.v);
+
+console.log("Detail Nodejs: ", entorn.vdetail);
 ```
--- examples/entorn_example.js
+++ examples/entorn_example.js
@@ -1,21 +1,45 @@
 var entorn = require('../index');
 
-console.log("List:" + JSON.stringify(entorn.list()));
+console.log("List all environment variables:" + entorn.list());
 
-console.log("java is installed? " + entorn.hasJavaHome());
+console.log("CPUs: ", entorn.cpus); // list of all CPU
 
-console.log("where java is installed? " + entorn.getJavaHome());
+console.log("Free memory: ", entorn.free);
 
-console.log("What is the name of the user of this computer? ", entorn.User());
+console.log("Similary: ", entorn.similary("NODE")); // list of all variables similar environment
 
-console.log("You have it set android? ", entorn.hasAndroidHome());
+console.log("have java installed? " + entorn.hasJavaHome()); // true | false
 
-console.log("has axs?", entorn.has("axs"));
+console.log("where java is installed? " + entorn.getJavaHome()); // string dir folder java
 
-console.log("or, has SESSION?", entorn.has("SESSION"));
+console.log("What is the name of the user of this computer? ", entorn.User()); // username of the session
 
-console.log("get NODE_PATH", entorn.get("NODE_PATH"));
+console.log("You have it set android? ", entorn.hasAndroidHome()); // true | false
 
-console.log("Similary: ", entorn.similary("JAVA"));
+console.log("has axs?", entorn.has("axs")); // true | false
 
-// console.log("Set: ", entorn.set("YOUR_ENV_VAR", "ANY VALUES"));
+console.log("or, has SESSION?", entorn.has("SESSION")); // true | false
+
+console.log("get NODE_PATH", entorn.get("NODE_PATH")); // string dir folder
+
+//console.log("Set: ", entorn.set("YOUR_ENV_VAR", "ANY VALUES"));
+
+console.log("CPUs: ", entorn.cpus);
+
+console.log("Free memory: ", entorn.free);
+
+console.log("Network Interfaces: ", entorn.networkInterfaces);
+
+console.log("or Network Interfaces: ", entorn.ni);
+
+console.log("Platform: ", entorn.platform); // 'aix', 'darwin', 'freebsd', 'linux', 'openbsd', 'sunos', 'win32'
+
+console.log("Total memory: ", entorn.totalmem);
+
+console.log("Current folder: ", entorn.current); // Returns the current working directory of the Node.js process.  Similiar to command "pwd" on linux
+
+console.log("Version nodejs: ", entorn.version);
+
+console.log("or Version nodejs: ", entorn.v);
+
+console.log("Detail Nodejs: ", entorn.vdetail);
